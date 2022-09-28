d38b3f5a1fa35661fe86f850bdbf0b123598d521
yima <1320316068@qq.com>
2022-09-28 19:00:37 +0800

    feat:工单和销售样式修改

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -6,25 +6,55 @@
           <div class="grid-content sku-sale-data">
             <el-row>
               <!-- 工单统计区域 -->
-              <el-col :span="15">
+              <el-col :span="13">
                 <div class="grid-content home-user-task-stats">
                   <!-- 标题区域 -->
                   <div class="home-title">
-                    <span>   工单统计</span>
+                    <span>工单统计</span>
                     <span class="home-data-time">2022.09.01~2022.09.28</span>
                   </div>
-
+                  <!-- 内容区域 -->
+                  <el-row class="home-user-task-stats-msg">
+                    <el-col :span="6" class="home-task-stats-item">
+                      <span class="task-stats-data">150</span>
+                      <span class="home-task-stats-text">工单总数（个）</span>
+                    </el-col>
+                    <el-col :span="6" class="home-task-stats-item">
+                      <span class="task-stats-data">0</span>
+                      <span class="home-task-stats-text">完成工单（个）</span>
+                    </el-col>
+                    <el-col :span="6" class="home-task-stats-item">
+                      <span class="task-stats-data">0</span>
+                      <span class="home-task-stats-text">进行工单（个）</span>
+                    </el-col>
+                    <el-col :span="6" class="home-task-stats-item">
+                      <span class="task-stats-data">150</span>
+                      <span class="home-task-stats-text">取消工单（个）</span>
+                    </el-col>
+                  </el-row>
                 </div>
               </el-col>
 
               <!-- 销售统计区域 -->
-              <el-col :span="9"><div class="grid-content home-sku-sale-stats">
+              <el-col :span="11"><div class="grid-content home-sku-sale-stats">
                 <!-- 标题区域 -->
                 <div class="home-title">
                   <span>   销售统计</span>
                   <span class="home-data-time">2022.09.01~2022.09.28</span>
                 </div>
+                <!-- 内容区域 -->
+                <el-row class="home-user-task-stats-msg">
 
+                  <el-col :span="12" class="home-task-stats-item">
+                    <span class="task-stats-data">13504</span>
+                    <span class="home-task-stats-text">订单量（个）</span>
+                  </el-col>
+                  <el-col :span="12" class="home-task-stats-item">
+                    <span class="task-stats-data">8.81</span>
+                    <span class="home-task-stats-text">销售额（万元）</span>
+                  </el-col>
+
+                </el-row>
               </div>
               </el-col>
             </el-row>
@@ -81,7 +111,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import * as echarts from 'echarts'
+
 export default {
   name: 'Dashboard',
   computed: {
@@ -90,24 +120,7 @@ export default {
     ])
   },
   mounted() {
-    var myChart = echarts.init(document.querySelector('.home-user-task-stats'))
-    myChart.setOption({
-      title: {
-        text: 'ECharts 入门示例'
-      },
-      tooltip: {},
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      },
-      yAxis: {},
-      series: [
-        {
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }
-      ]
-    })
+
   }
 }
 </script>
@@ -161,6 +174,30 @@ export default {
     color: #999;
     font-size: 12px;
   }
+        .home-user-task-stats-msg{
+
+        display: flex;
+        align-items: center;
+      min-height: 108px;
+     .home-task-stats-item {
+      padding: 20px;
+      display: flex;
+      flex-direction: column;
+      // text-align: center;
+       height: 100%;
+       .task-stats-data {
+
+        font-size: 36px;
+        font-weight: 700;
+
+       }
+       .home-task-stats-text {
+        margin-top: 10px;
+
+            font-size: 12px;
+       }
+        }
+      }
   // 商品热榜区域
   .sku-sale-rank {
     padding: 10px;
@@ -173,20 +210,61 @@ export default {
      min-height: 540px;
     //  工单统计区域
      .home-user-task-stats {
-
       padding: 20px;
       min-height: 166px;
       margin-right: 20px;
       background: #e9f3ff;
       background-image: url('~@/assets/common/circle.png'), url('~@/assets/common/task.png');
       background-repeat: no-repeat no-repeat ;
-      background-position: 0 0,calc(100% - 12px) 100%
+      background-position: 0 0,calc(100% - 12px) 100%;
+    //   .home-user-task-stats-msg{
+
+    //     display: flex;
+    //     align-items: center;
+    //   min-height: 108px;
+    //  .home-task-stats-item {
+    //        padding: 20px;
+    //   display: flex;
+    //   flex-direction: column;
+    //   // text-align: center;
+    //    height: 100%;
+    //    .task-stats-data {
+    //     color: #072074;
+    //     font-size: 36px;
+    //     font-weight: 700;
+    //         text-shadow: 2px 4px 7px rgb(85 132 255 / 50%)
+    //    }
+    //    .home-task-stats-text {
+    //     margin-top: 10px;
+    //         color: #91a7dc;
+    //         font-size: 12px;
+    //    }
+    //     }
+    //   }
+       .task-stats-data {
+        color: #072074;
+       }
+      .home-task-stats-text {
+        color: #91a7dc;
+       }
+
      }
     //  销售统计区域
      .home-sku-sale-stats {
       padding: 20px;
          background:  #fbefe8 url('~@/assets/common/pinkpig.png') no-repeat calc(100% - 12px) 100%;
          min-height: 166px;
+          .home-task-stats-item {
+       padding-left: 70px;
+          }
+    .task-stats-data {
+      color: #ff5757;
+      text-shadow: 2px 4px 7px rgb(255 99 85 / 50%)
+      }
+        .home-task-stats-text {
+        color: #de9690;
+       }
+
      }
     //  销售数据区域
        .sku-sale-collect {
